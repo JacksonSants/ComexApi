3023a1968acdc9e49ce36232184aa31d35b1cf43
JacksonSants <jacksondiego2002@gmail.com>
2024-09-05 20:22:48 -0300

    CRUD com lista estática e validação dos campos.

---
+++ ComexApi/Controllers/ProdutoController.cs
@@ -0,0 +1,56 @@
+﻿using ComexApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComexApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProdutoController : ControllerBase
+{
+    private static List<Produto> produtos = new List<Produto>();
+
+    [HttpPost]
+    public void CadastrarProduto([FromBody] Produto produto)
+    {
+        produtos.Add(produto);
+
+    }
+
+    [HttpGet]
+    public IEnumerable<Produto> ConsultarProdutos()
+    {
+        return produtos;
+    }
+
+    [HttpPut("{nome}")]
+    public IActionResult AtualizarProduto(string nome, [FromBody] Produto produto)
+    {
+        var produtoExistente = produtos.FirstOrDefault(produto => produto.Nome == nome);
+        if (produtoExistente == null)
+        {
+            return NotFound();
+        }
+
+        produtoExistente.Nome = produto.Nome;
+        produtoExistente.Descricao = produto.Descricao;
+        produtoExistente.PrecoUnitario = produto.PrecoUnitario;
+        produtoExistente.Quantidade = (int)produto.Quantidade;
+
+        return Ok(produtoExistente);
+
+    }
+
+    [HttpDelete("{nome}")]
+    public IActionResult RemoverProduto(string nome)
+    {
+        var produtoExistente = produtos.FirstOrDefault(produto => produto.Nome == nome);
+        if (produtoExistente == null)
+        {
+            return NotFound();
+        }
+
+        produtos.Remove(produtoExistente);
+        return Ok(nome);
+    }
+
+}
---
+++ ComexApi/Models/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ComexApi.Models;
+
+public class Produto
+{
+    [Required(ErrorMessage = "O Nome do produto é obrigatório.")]
+    [MaxLength(100, ErrorMessage = "O Nome do produto deve ter até 100 caracteres.")]
+    public string Nome { get; set; }
+
+    [MaxLength(500, ErrorMessage = "A descrição do produto deve ter até 500 caracteres.")]
+    public string Descricao { get; set; }
+
+    [Required(ErrorMessage = "O PrecoUnitario é obrigatório.")]
+    public double PrecoUnitario { get; set; }
+
+    [Required(ErrorMessage = "A Quantidade é obrigatória.")]
+    public int Quantidade { get; set; }
+}
