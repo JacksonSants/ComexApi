0b8d2f6763480a57fbd4938ccfa07d2020d46218
JacksonSants <jacksondiego2002@gmail.com>
2024-09-05 22:39:39 -0300

    Documentação da API de produtos através do Swagger.

--- ComexApi/Controllers/ProdutoController.cs
+++ ComexApi/Controllers/ProdutoController.cs
@@ -19,7 +19,13 @@ public class ProdutoController : ControllerBase
         _mapper = mapper;
     }
 
+    /// <summary>
+    /// Adiciona um produto ao banco de dados.
+    /// </summary>
+    /// <param name="produtoDto">Objetos necessários para a criação de um produto.</param>
+    /// <response code="201">Caso a inserção seja feita com sucesso.</response>
     [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
     public void CadastrarProduto([FromBody] CreateProdutoDto produtoDto)
     {
         Produto produto = _mapper.Map<Produto>(produtoDto);
@@ -27,7 +33,13 @@ public class ProdutoController : ControllerBase
         _context.SaveChanges();
     }
 
+    /// <summary>
+    /// Recupera os produtos do banco de dados.
+    /// </summary>
+    /// <param name="produtoDto">.</param>
+    /// <response code="200">Caso a recuperação seja feita com sucesso.</response>
     [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
     public IEnumerable<ReadProdutoDto> ConsultarProdutos()
     {
         return _mapper.Map<List<ReadProdutoDto>>(_context.Produtos);
@@ -43,7 +55,13 @@ public class ProdutoController : ControllerBase
         }
     }*/
 
+    /// <summary>
+    /// Atualiza os dados de um produto do  banco de dados através de um parâmetro {id}.
+    /// </summary>
+    /// <param name="produtoDto">Objetos necessários para a atualização de um produto.</param>
+    /// <response code="204">Caso a atualização seja feita com sucesso.</response>
     [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
     public IActionResult AtualizarProduto(int id, [FromBody]  UpdateProdutoDto produtoDto)
     {
         var produto = _context.Produtos.FirstOrDefault(produto => produto.Id == id);
@@ -57,7 +75,13 @@ public class ProdutoController : ControllerBase
 
     }
 
+    /// <summary>
+    /// Remove um produto do banco de dados através de um parâmtro {id}.
+    /// </summary>
+    /// <param name="produtoDto"></param>
+    /// <response code="200">Caso a remoção seja feita com sucesso.</response>
     [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
     public IActionResult RemoverProduto(int id)
     {
         var produtoExistente = _context.Produtos.FirstOrDefault(produto => produto.Id == id);
--- ComexApi/Program.cs
+++ ComexApi/Program.cs
@@ -1,5 +1,7 @@
 using ComexApi.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 var produtoConnection = builder.Configuration.GetConnectionString("ProdutoConnection");
@@ -13,6 +15,14 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProdutoAPI", Version = "v1" });
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile); 
+    c.IncludeXmlComments(xmlPath);
+});
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
