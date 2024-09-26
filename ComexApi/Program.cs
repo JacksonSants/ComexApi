89823074d109da1c5e789fd91935397713a18a91
Jackson Ribeiro <jacksonribeiro@bemol.com.br>
2024-09-26 11:45:24 -0400

    Documentação da rota categoria.

--- ComexApi/Program.cs
+++ ComexApi/Program.cs
@@ -1,5 +1,6 @@
 using ComexApi.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using System.Reflection;
 
@@ -17,9 +18,25 @@ builder.Services.AddControllers().AddNewtonsoftJson();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProdutoAPI", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Comex API",
+        Description = "API de E-commerce para vendas de produtos.",
+        TermsOfService = new Uri("https://localhost:7118/swagger/index.html"),
+        Contact = new OpenApiContact
+        {
+            Name = "jacksondiego@gmail.com",
+            Url = new Uri("https://localhost:7118/swagger/index.html")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Documentação",
+            Url = new Uri("https://localhost:7118/swagger/index.html")
+        }
+    });
     var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile); 
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
     c.IncludeXmlComments(xmlPath);
 });
 
